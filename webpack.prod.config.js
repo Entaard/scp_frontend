76f1861c744f2e1a2530e967d8916e7cc66d2abf
Quan Vu <nongdenchet8435848@gmail.com>
2017-01-04 18:35:20 +0700

    fix config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -39,7 +39,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        loader: 'style-loader!css-loader!sass-loader'
+        loaders: ["style-loader", "css-loader", "sass-loader"]
       }
     ]
   },

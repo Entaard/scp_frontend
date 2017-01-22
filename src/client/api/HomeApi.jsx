ec5e92076ae48677e764f4934c797a559e8dccea
Quan Vu <nongdenchet8435848@gmail.com>
2017-01-22 10:05:03 +0700

    add sample mock

--- src/client/api/HomeApi.jsx
+++ src/client/api/HomeApi.jsx
@@ -1,7 +1,12 @@
 import axios from 'axios'
+var MockAdapter = require('axios-mock-adapter')
+var mock = new MockAdapter(axios)
 
 const HomeApi = {
   getHome() {
+    mock.onGet('/home').reply(200, {
+      success: 'hello world'
+    })
     return axios.get('/home')
   }
 }
--- src/client/containers/Home.jsx
+++ src/client/containers/Home.jsx
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import BlogItem from '../components/BlogItem'
 import Product from '../components/Product'
 import Swiper from '../components/Swiper'
 import InstaFeed from '../components/InstaFeed'
@@ -25,90 +24,90 @@ export class Home extends Component {
 
   renderProductItem() {
     return (
-        items.map((item, index) => (
-            <Product
-                key={item.id}
-                name={item.name}
-                price={item.price}
-            />
-        ))
+      items.map((item, index) => (
+        <Product
+          key={item.id}
+          name={item.name}
+          price={item.price}
+        />
+      ))
     )
   }
 
   render() {
     return (
-        <div className="page-main">
-          <div className="block fullwidth full-nopad bottom-space">
-            <div className="container">
-              <Swiper/>
-            </div>
+      <div className="page-main">
+        <div className="block fullwidth full-nopad bottom-space">
+          <div className="container">
+            <Swiper/>
           </div>
-          <div className="block">
-            <div className="container">
-              <div className="text-center bottom-space">
-                <h1 className="size-lg no-padding">WELCOME TO <span className="logo-font custom-color">Seikō</span>
-                  STORE</h1>
-                <div className="line-divider"></div>
-                <p className="custom-color-alt">Lorem ipsum dolor sit amet, ex eam mundi populo accusamus, aliquam
-                  quaestio
-                  petentium te cum.
-                  <br/> Vim ei oblique tacimates, usu cu iudico graeco. Graeci eripuit inimicus vel eu, eu mel unum
-                  laoreet splendide, cu integre apeirian has.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="block">
-            <div className="container">
-              <div className="row">
-                <div className="col-sm-4">
-                  <div className="box style2 bgcolor1 text-center">
-                    <div className="box-icon"><i className="icon icon-truck"></i></div>
-                    <h3 className="box-title">FREE SHIPPING</h3>
-                    <div className="box-text">Free shipping on all orders over $199</div>
-                  </div>
-                </div>
-                <div className="col-sm-4">
-                  <div className="box style2 bgcolor2 text-center">
-                    <div className="box-icon"><i className="icon icon-dollar"></i></div>
-                    <h3 className="box-title">MONEY BACK</h3>
-                    <div className="box-text">100% money back guarantee</div>
-                  </div>
-                </div>
-                <div className="col-sm-4">
-                  <div className="box style2 bgcolor3 text-center">
-                    <div className="box-icon"><i className="icon icon-help"></i></div>
-                    <h3 className="box-title">ONLINE SUPPORT</h3>
-                    <div className="box-text">Service support fast 24/7</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="block">
-            <div className="container">
-              <ul className="filters filters-product style2">
-                <li><a 
-                       className="filter-label">All<span className="count"></span></a></li>
-                <li><a 
-                       className="filter-label active"
-                       data-filter=".category1">New<span
-                    className="count"></span></a>
-                </li>
-                <li><a 
-                       className="filter-label"
-                       data-filter=".category2">Sale<span
-                    className="count"></span></a></li>
-              </ul>
-              <div className="products-grid-wrapper isotope-wrapper">
-                <div className="products-grid isotope four-in-row product-variant-4">
-                  {this.renderProductItem()}
-                </div>
-              </div>
-            </div>
-          </div>
-          <InstaFeed />
         </div>
+        <div className="block">
+          <div className="container">
+            <div className="text-center bottom-space">
+              <h1 className="size-lg no-padding">WELCOME TO <span className="logo-font custom-color">Seikō</span>
+                STORE</h1>
+              <div className="line-divider"></div>
+              <p className="custom-color-alt">Lorem ipsum dolor sit amet, ex eam mundi populo accusamus, aliquam
+                quaestio
+                petentium te cum.
+                <br/> Vim ei oblique tacimates, usu cu iudico graeco. Graeci eripuit inimicus vel eu, eu mel unum
+                laoreet splendide, cu integre apeirian has.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="block">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-4">
+                <div className="box style2 bgcolor1 text-center">
+                  <div className="box-icon"><i className="icon icon-truck"></i></div>
+                  <h3 className="box-title">FREE SHIPPING</h3>
+                  <div className="box-text">Free shipping on all orders over $199</div>
+                </div>
+              </div>
+              <div className="col-sm-4">
+                <div className="box style2 bgcolor2 text-center">
+                  <div className="box-icon"><i className="icon icon-dollar"></i></div>
+                  <h3 className="box-title">MONEY BACK</h3>
+                  <div className="box-text">100% money back guarantee</div>
+                </div>
+              </div>
+              <div className="col-sm-4">
+                <div className="box style2 bgcolor3 text-center">
+                  <div className="box-icon"><i className="icon icon-help"></i></div>
+                  <h3 className="box-title">ONLINE SUPPORT</h3>
+                  <div className="box-text">Service support fast 24/7</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="block">
+          <div className="container">
+            <ul className="filters filters-product style2">
+              <li><a
+                className="filter-label">All<span className="count"></span></a></li>
+              <li><a
+                className="filter-label active"
+                data-filter=".category1">New<span
+                className="count"></span></a>
+              </li>
+              <li><a
+                className="filter-label"
+                data-filter=".category2">Sale<span
+                className="count"></span></a></li>
+            </ul>
+            <div className="products-grid-wrapper isotope-wrapper">
+              <div className="products-grid isotope four-in-row product-variant-4">
+                {this.renderProductItem()}
+              </div>
+            </div>
+          </div>
+        </div>
+        <InstaFeed />
+      </div>
     )
   }
 }

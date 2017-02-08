332850aa19a196d13bf2da3e9525a78be445d3d8
Nguyen Minh Khoi <minhkhoi28@yahoo.com>
2017-02-08 21:04:00 +0700

    wizard config again

--- src/client/containers/admin/wizard/Wizard.jsx
+++ src/client/containers/admin/wizard/Wizard.jsx
@@ -25,9 +25,7 @@ export class Wizard extends Component {
     //truyen them Id cua product cho cac step 2 3 4
     switch (this.state.step) {
       case 1:
-        // return <Info nextStep={this.nextStep}/>
-        return <Option
-          previousStep={this.previousStep}/>
+        return <Info nextStep={this.nextStep}/>
       case 2:
         return <Training
           nextStep={this.nextStep}

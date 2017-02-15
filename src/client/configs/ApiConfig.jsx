19638cad8f1d6eebe5c8b4862cbee50b551aff9d
Quan Vu <nongdenchet8435848@gmail.com>
2017-02-15 19:25:47 +0700

    update url

--- src/client/configs/ApiConfig.jsx
+++ src/client/configs/ApiConfig.jsx
@@ -5,7 +5,7 @@ import { isDevelopment } from '../configs/EnvConfig'
 
 export function rootUrl() {
   if (isDevelopment()) {
-    return "https://localhost:3000/"
+    return "http://localhost:3000/"
   } else {
     return "https://scp-backend.herokuapp.com/"
   }
